54e65920bfa40d2c1edc7284be1e7a46372f9f75
Alexey Ershov <alex4rom@gmail.com>
2017-06-21 16:45:25 +0300

    Stub state

--- app/index.js
+++ app/index.js
@@ -1,4 +1,21 @@
 import { createStore } from 'redux';
 
+const initialState = {
+    recipes: [
+        {
+            name: 'first'
+        }
+    ],
+    ingredients: [
+        {
+            recipe: 'first',
+            name: 'test1',
+            quantity: 2
+        }
+    ]
+}
+
 const reducer = (state, action) => state;
-const store = createStore(reducer);
+const store = createStore(reducer, initialState);
+
+window.store = store;
