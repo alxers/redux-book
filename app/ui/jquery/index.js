32b76207858399801b20ee7e64c84fa2b53a6a48
Alexey Ershov <alex4rom@gmail.com>
2017-09-22 21:07:08 +0300

    Update index.js

--- app/ui/jquery/index.js
+++ app/ui/jquery/index.js
@@ -7,3 +7,14 @@ function updateUI() {
 
   $('.recipes > ul').html(recipes.map(renderRecipe));
 }
+
+export default function loadUI() {
+  $('#app').append(`
+    <div class="recipes">
+    <h2>Recipes:</h2>
+    <ul></ul>
+    </div>
+   `);
+  
+   updateUI();
+}
