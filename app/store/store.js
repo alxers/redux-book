28c20dd142c1cb4694cd0c03a545ee18966dcf22
Alexey Ershov <alex4rom@gmail.com>
2017-09-27 20:31:12 +0300

    Update store

--- app/store/store.js
+++ app/store/store.js
@@ -1,3 +1,8 @@
 import { createStore, applyMiddleware } from 'redux';
 import rootReducers from 'reducers/root';
 import logMiddleware from 'middleware/log';
+
+export default createStore(
+  rootReducers,
+  applyMiddleware(logMiddleware, apiMiddleware)
+);
